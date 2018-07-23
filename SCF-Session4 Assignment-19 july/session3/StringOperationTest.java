912a96dd434e73708d8dabaca88960360e096fb4
meta-mohammed-jukker <41181769+meta-mohammed-jukker@users.noreply.github.com>
2018-07-23 16:14:57 +0530

    Update StringOperationTest.java

--- SCF-Session4 Assignment-19 july/session3/StringOperationTest.java
+++ SCF-Session4 Assignment-19 july/session3/StringOperationTest.java
@@ -1,73 +1,199 @@
-package session3;
+package session4;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class StringOperationTest
+public class ArrOperationTest
 {
-    StringOperation stringOperation = new StringOperation();
-
-    //Tests for comparingString()
-    @Test
-    public void testComparingStringWithEqualArguments()
+	
+	ArrOperation arrOperation = new ArrOperation();
+	
+	
+	// Tests for splitListOfNumbers()
+	@Test
+	public void testSplitListOfNumbersWithSplitAtIndex3()
+	{
+	    int[] inputArray = {1, 1, 1, 2, 1};
+	    int expectedOutput = 3;
+		assertEquals(expectedOutput, arrOperation.splitListOfNumbers(inputArray));
+	}
+	
+	@Test
+    public void testSplitListOfNumbersWithNoSplit()
     {
-        String expectedString1 = "Mohammed";
-        String expectedString2 = "Mohammed";
+        int[] inputArray = {2, 1, 1, 2, 1};
+        int expectedOutput = -1;
+        assertEquals(expectedOutput, arrOperation.splitListOfNumbers(inputArray));
+    }
+	
+	@Test
+    public void testSplitListOfNumbersWithSplitAtIndex1()
+    {
+        int[] inputArray = {10, 10};
         int expectedOutput = 1;
-        assertEquals(expectedOutput, stringOperation.comparingString(expectedString1, expectedString2));
+        assertEquals(expectedOutput, arrOperation.splitListOfNumbers(inputArray));
+    }
+	
+	/**
+     * to test if array is empty
+     */
+	@Test(expected = AssertionError.class )
+    public void testSpLitlistOfNumbersWithEmptyArray()
+    {
+        int[] inputArray = {};
+        arrOperation.splitListOfNumbers(inputArray);
+    }
+	
+	
+	//Tests for fixXY()
+	@Test
+	public void testFixXY1()
+	{
+	    int[] inputArray = {5, 4, 9, 4, 9, 5};
+	    int[] expectedArray = {9, 4, 5, 4, 5, 9};
+	    assertArrayEquals(expectedArray, arrOperation.fixXY(inputArray, 4, 5));
+	}
+	
+	@Test
+    public void testFixXY2()
+    {
+        int[] inputArray = {1, 3, 1, 2};
+        int[] expectedArray = {1, 3, 2, 1};
+        assertArrayEquals(expectedArray, arrOperation.fixXY(inputArray, 3, 2));
+    }
+	
+	@Test
+    public void testFixXY3()
+    {
+        int[] inputArray = {1, 0, 1, 9, 9, 0, 1};
+        int[] expectedArray = {1, 0, 9, 1, 1, 0, 9};
+        assertArrayEquals(expectedArray, arrOperation.fixXY(inputArray, 0, 9));
+    }
+	
+	/**
+	 * to test if occurrences of X and Y are equal
+	 */
+	@Test(expected = AssertionError.class )
+    public void testFixXYWithEqualNumberOfXY()
+    {
+	    int[] inputArray = {1,3,1,3,1,2,5,2,2};
+	    arrOperation.fixXY(inputArray, 3, 2);
+    }
+	
+	/**
+	 * to test if array is empty
+	 */
+	@Test(expected = AssertionError.class )
+    public void testFixXYWithEmptyArray()
+    {
+        int[] inputArray = {};
+        arrOperation.fixXY(inputArray, 3, 2);
+    }
+	
+	/**
+	 * to test if X element is at the end
+	 */
+	@Test(expected = AssertionError.class )
+    public void testFixXYWithXAtEnd()
+    {
+        int[] inputArray = {1,3,2,3,2,1};
+        arrOperation.fixXY(inputArray, 1, 2);
+    }
+	
+	
+	// Tests for countClumps()
+    @Test
+    public void testCountClumpsWith2Clumps()
+    {
+        int[] inputArray = {1, 2, 2, 3, 4, 4};
+        int expectedOutput = 2;
+        assertEquals(expectedOutput, arrOperation.countClumps(inputArray));
     }
     
     @Test
-    public void testComparingStringWithNotEqualArguments()
+    public void testCountClumpsWith3Clumps()
     {
-        String expectedString1 = "Mohammed";
-        String expectedString2 = "Jukker";
+        int[] inputArray = {1, 1, 2, 2, 1, 1, 2};
+        int expectedOutput = 3;
+        assertEquals(expectedOutput, arrOperation.countClumps(inputArray));
+    }
+    
+    @Test
+    public void testCountClumpsWith1Clump()
+    {
+        int[] inputArray = {1, 1, 1, 1, 1};
+        int expectedOutput = 1;
+        assertEquals(expectedOutput, arrOperation.countClumps(inputArray));
+    }
+    
+    @Test
+    public void testCountClumpsWithNoClump()
+    {
+        int[] inputArray = {1, 2, 3, 4, 5};
         int expectedOutput = 0;
-        assertEquals(expectedOutput, stringOperation.comparingString(expectedString1, expectedString2));
+        assertEquals(expectedOutput, arrOperation.countClumps(inputArray));
+    }
+    
+    /**
+     * to test if array is empty
+     */
+    @Test(expected = AssertionError.class )
+    public void testCountClumpsWithEmptyArray()
+    {
+        int[] inputArray = {};
+        arrOperation.countClumps(inputArray);
+    }
+    
+    
+    // Tests for findMaxMirror()
+    @Test
+    public void testFindMaxMirror1()
+    {
+        int[] inputArray = {1, 2, 3, 8, 9, 3, 2, 1};
+        int expectedOutput = 3;
+        assertEquals(expectedOutput, arrOperation.findMaxMirror(inputArray));
     }
     
     @Test
-    public void testComparingStringWithCaseSensitiveArguments()
+    public void testFindMaxMirror2()
     {
-        String expectedString1 = "Mohammed";
-        String expectedString2 = "mohammed";
+        int[] inputArray = {7, 1, 4, 9, 7, 4, 1};
+        int expectedOutput = 2;
+        assertEquals(expectedOutput, arrOperation.findMaxMirror(inputArray));
+    }
+    
+    @Test
+    public void testFindMaxMirror3()
+    {
+        int[] inputArray = {1, 2, 1, 4};
+        int expectedOutput = 3;
+        assertEquals(expectedOutput, arrOperation.findMaxMirror(inputArray));
+    }
+    
+    @Test
+    public void testFindMaxMirror4()
+    {
+        int[] inputArray = {1, 4, 5, 3, 5, 4, 1};
+        int expectedOutput = 7;
+        assertEquals(expectedOutput, arrOperation.findMaxMirror(inputArray));
+    }
+    
+    @Test
+    public void testFindMaxMirror5()
+    {
+        int[] inputArray = {1, 2, 3, 4, 5, 6};
         int expectedOutput = 0;
-        assertEquals(expectedOutput, stringOperation.comparingString(expectedString1, expectedString2));
+        assertEquals(expectedOutput, arrOperation.findMaxMirror(inputArray));
     }
     
-    //Tests for reversingString()
-    @Test
-    public void testReversingString()
+    /**
+     * to test if array is empty
+     */
+    @Test(expected = AssertionError.class )
+    public void testFindMaxMirrorWithEmptyArray()
     {
-        String expectedString = "Mohammed";
-        String expectedOutput = "demmahoM";
-        assertEquals(expectedOutput, stringOperation.reversingString(expectedString));
-    }
-    
-    //Tests for changingCase()
-    @Test
-    public void testChangingCase()
-    {
-        String expectedString = "Mohammed29";
-        String expectedOutput = "mOHAMMED29";
-        assertEquals(expectedOutput, stringOperation.changingCase(expectedString));
-    }
-    
-    //Tests for largestWord()
-    @Test
-    public void testlargestWord()
-    {
-        String expectedString = "abc de fghi jkl mnop";
-        String expectedOutput = "mnop";
-        assertEquals(expectedOutput, stringOperation.largestWord(expectedString));
-    }
-    
-    @Test
-    public void testlargestWordWithNoWords()
-    {
-        String expectedString = "    ";
-        String expectedOutput = "";
-        assertEquals(expectedOutput, stringOperation.largestWord(expectedString));
+        int[] inputArray = {};
+        arrOperation.findMaxMirror(inputArray);
     }
 }
