698fe51b21b767debb926fa3c48f59a83f657bb5
meta-mohammed-jukker <41181769+meta-mohammed-jukker@users.noreply.github.com>
2018-08-13 14:21:57 +0530

    Update DriverClass.java

--- DSA-Session6 Assignment-8 august/undirectedWeightedGraph/DriverClass.java
+++ DSA-Session6 Assignment-8 august/undirectedWeightedGraph/DriverClass.java
@@ -5,29 +5,29 @@ public class DriverClass
 
     public static void main(String[] args)
     {
-        UndirectedWeightedGraph uwg = new UndirectedWeightedGraph(6);
-        uwg.addEgde(0, 1, 4);
-        uwg.addEgde(0, 2, 3);
-        uwg.addEgde(1, 3, 2);
-        uwg.addEgde(1, 2, 5);
-        uwg.addEgde(2, 3, 7);
-        uwg.addEgde(3, 4, 2);
-        uwg.addEgde(4, 0, 8);
-        uwg.addEgde(4, 1, 4);
-        uwg.addEgde(4, 5, 6);
-        uwg.printGraph();
-        System.out.println(uwg.isConnected());
-        for(int i: uwg.reachable(0))
+        UndirectedWeightedGraph undirectedWeightedGraph = new UndirectedWeightedGraph(6);
+        undirectedWeightedGraph.addEgde(0, 1, 4);
+        undirectedWeightedGraph.addEgde(0, 2, 3);
+        undirectedWeightedGraph.addEgde(1, 3, 2);
+        undirectedWeightedGraph.addEgde(1, 2, 5);
+        undirectedWeightedGraph.addEgde(2, 3, 7);
+        undirectedWeightedGraph.addEgde(3, 4, 2);
+        undirectedWeightedGraph.addEgde(4, 0, 8);
+        undirectedWeightedGraph.addEgde(4, 1, 4);
+        undirectedWeightedGraph.addEgde(4, 5, 6);
+        undirectedWeightedGraph.printGraph();
+        System.out.println(undirectedWeightedGraph.isConnected());
+        for(int i: undirectedWeightedGraph.reachable(0))
         {
             System.out.print(i + " ");
         }
         System.out.println();
-        for(Edge edge: uwg.getMinimumSpanningTree())
+        for(Edge edge: undirectedWeightedGraph.getMinimumSpanningTree())
         {
             System.out.println(edge.getSource() + " " + edge.getDestination() + " " + edge.getWeight());
         }
         System.out.println("\n");
-        for(Edge edge: uwg.getShortestPath(2, 5))
+        for(Edge edge: undirectedWeightedGraph.getShortestPath(2, 5))
         {
             System.out.println(edge.getSource() + " " + edge.getDestination() + " " + edge.getWeight());
         }
