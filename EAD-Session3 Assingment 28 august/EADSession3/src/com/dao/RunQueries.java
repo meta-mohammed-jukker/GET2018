aff4f737f8538218d3c99f5058bb32bdf6d55ec7
meta-mohammed-jukker <41181769+meta-mohammed-jukker@users.noreply.github.com>
2018-08-30 13:16:47 +0530

    Update RunQueries.java

--- EAD-Session3 Assingment 28 august/EADSession3/src/com/dao/RunQueries.java
+++ EAD-Session3 Assingment 28 august/EADSession3/src/com/dao/RunQueries.java
@@ -158,6 +158,97 @@ public class RunQueries
     }
     
     /**
+     * Update Employee details of employee with given employeeID
+     * @param employee employee object
+     * @return true if employee updated, else false
+     */
+    public boolean updateEmployeeDetails(Employee employee)
+    {
+        int isEmployeeUpdated = 0;
+        
+        String query = Queries.updateEmployee;
+        
+        try
+        (
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+        )
+        {
+            preparedStatement.setString(1, employee.getFirstName());
+            preparedStatement.setString(2, employee.getLastName());
+            preparedStatement.setString(3, employee.getEmail());
+            preparedStatement.setInt(4, employee.getAge());
+            preparedStatement.setInt(5, employee.getId());
+            isEmployeeUpdated = preparedStatement.executeUpdate();
+        } 
+        catch (SQLException sqlException)
+        {
+            sqlException.printStackTrace();
+            System.exit(1);
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+            System.exit(2);
+        }
+        finally
+        {
+            dispose();
+        }
+
+        if(isEmployeeUpdated > 0)
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Returns Employee details of employee with given employeeID
+     * @param employeeID employee ID
+     * @return employee object
+     */
+    public Employee getEmployeeByID(int employeeID)
+    {
+        Employee employee = null;
+        String query = Queries.showEmployeeDetailsByID;
+        
+        try
+        (
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+        )
+        {
+            preparedStatement.setInt(1, employeeID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            while(resultSet.next())
+            {
+                String emailID = resultSet.getString("emailID");
+                String firstName = resultSet.getString("firstName");
+                String lastName = resultSet.getString("lastName");
+                int age = resultSet.getInt("age");
+                
+                employee = new Employee(employeeID, firstName, lastName, emailID, age);
+            }
+            resultSet.close();
+        } 
+        catch (SQLException sqlException)
+        {
+            sqlException.printStackTrace();
+            System.exit(1);
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+            System.exit(2);
+        }
+        finally
+        {
+            dispose();
+        }
+        return employee;
+    }
+    
+    /**
      * Closes connection
      */
     public void dispose()
