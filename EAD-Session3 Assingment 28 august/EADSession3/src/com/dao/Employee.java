e36730a4ee0a15c79938bdeca0ca33dd7f58a1e2
meta-mohammed-jukker <41181769+meta-mohammed-jukker@users.noreply.github.com>
2018-08-30 13:15:53 +0530

    Update Employee.java

--- EAD-Session3 Assingment 28 august/EADSession3/src/com/dao/Employee.java
+++ EAD-Session3 Assingment 28 august/EADSession3/src/com/dao/Employee.java
@@ -25,41 +25,73 @@ public class Employee
         this.age = age;
     }
     
+    /**
+     * Returns employee ID
+     * @return
+     */
     public int getId()
     {
         return id;
     }
 
+    /**
+     * Sets employee ID
+     * @param id
+     */
     public void setId(int id)
     {
         this.id = id;
     }
     
+    /**
+     * Returns employee first name
+     * @return
+     */
     public String getFirstName()
     {
         return firstName;
     }
 
+    /**
+     * Sets employee first name
+     * @param firstName
+     */
     public void setFirstName(String firstName)
     {
         this.firstName = firstName;
     }
 
+    /**
+     * Returns employee last name
+     * @return
+     */
     public String getLastName()
     {
         return lastName;
     }
 
+    /**
+     * Sets employee last name
+     * @param lastName
+     */
     public void setLastName(String lastName)
     {
         this.lastName = lastName;
     }
 
+    /**
+     * Returns employee email ID
+     * @return
+     */
     public String getEmail()
     {
         return email;
     }
 
+    /**
+     * Sets employee email ID
+     * @param email
+     */
     public void setEmail(String email)
     {
         this.email = email;
@@ -70,6 +102,10 @@ public class Employee
         return age;
     }
 
+    /**
+     * Sets employee age
+     * @param age
+     */
     public void setAge(int age)
     {
         this.age = age;
