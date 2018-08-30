a445878e37b920efba358a253804ed08392f1a54
meta-mohammed-jukker <41181769+meta-mohammed-jukker@users.noreply.github.com>
2018-08-30 13:16:23 +0530

    Update Queries.java

--- EAD-Session3 Assingment 28 august/EADSession3/src/com/dao/Queries.java
+++ EAD-Session3 Assingment 28 august/EADSession3/src/com/dao/Queries.java
@@ -2,14 +2,25 @@ package com.dao;
 
 public class Queries
 {
-    static String addEmployee = "INSERT INTO employee(firstName, lastName, emailID, age) " + 
-                                "VALUES(?, ?, ?, ?)";
+    static final String addEmployee = "INSERT INTO employee(firstName, lastName, emailID, age) " + 
+                                      "VALUES(?, ?, ?, ?)";
     
-    static String showAllEmployees = "SELECT employeeID, firstName, lastName, emailID, age " +
-                                     "FROM employee ";
+    static final String showAllEmployees = "SELECT employeeID, firstName, lastName, emailID, age " +
+                                           "FROM employee ";
     
-    static String searchEmployee = "SELECT employeeID, firstName, lastName, emailID, age " +
-                                   "FROM employee " +
-                                   "WHERE CONCAT(firstName,' ',lastName) like CONCAT('%',?,'%') " + 
-                                   "OR CONCAT(lastName,' ',firstName) like CONCAT('%',?,'%')";
+    static final String searchEmployee = "SELECT employeeID, firstName, lastName, emailID, age " +
+                                         "FROM employee " +
+                                         "WHERE CONCAT(firstName,' ',lastName) like CONCAT('%',?,'%') " + 
+                                         "OR CONCAT(lastName,' ',firstName) like CONCAT('%',?,'%')";
+    
+    static final String updateEmployee = "Update employee " + 
+                                         "SET firstName = ?, " + 
+                                             "lastName = ?, " + 
+                                             "emailID = ?, " +
+                                             "age = ? " + 
+                                         "WHERE employeeID = ? ;";
+    
+    static final String showEmployeeDetailsByID = "SELECT employeeID, firstName, lastName, emailID, age " + 
+                                                  "FROM employee " + 
+                                                  "WHERE employeeID = ? ;";
 }
