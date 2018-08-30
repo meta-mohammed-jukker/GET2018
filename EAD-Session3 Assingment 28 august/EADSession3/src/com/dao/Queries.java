ea6bc61d795a3e031a2a218dcdce7bd884aa5275
meta-mohammed-jukker <41181769+meta-mohammed-jukker@users.noreply.github.com>
2018-08-30 10:37:02 +0530

    Update Queries.java

--- EAD-Session3 Assingment 28 august/EADSession3/src/com/dao/Queries.java
+++ EAD-Session3 Assingment 28 august/EADSession3/src/com/dao/Queries.java
@@ -7,4 +7,9 @@ public class Queries
     
     static String showAllEmployees = "SELECT employeeID, firstName, lastName, emailID, age " +
                                      "FROM employee ";
+    
+    static String searchEmployee = "SELECT employeeID, firstName, lastName, emailID, age " +
+                                   "FROM employee " +
+                                   "WHERE CONCAT(firstName,' ',lastName) like CONCAT('%',?,'%') " + 
+                                   "OR CONCAT(lastName,' ',firstName) like CONCAT('%',?,'%')";
 }
