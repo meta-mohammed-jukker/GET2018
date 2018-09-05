451756f8b78ff02a7de6753e1ada9ccf1ee4251e
meta-mohammed-jukker <41181769+meta-mohammed-jukker@users.noreply.github.com>
2018-09-05 16:05:16 +0530

    Add files via upload
    EAD-Session7 Assignment-3 september

---
+++ EAD-Session7 Assignment-3 september/EAD-Session7-Spring-AutowireByName/src/textEditor/Main.java
@@ -0,0 +1,19 @@
+package textEditor;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main
+{
+
+    public static void main(String args[])
+    {
+
+        @SuppressWarnings("resource")
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "beans.xml");
+
+        TextEditor textEditor = (TextEditor) context.getBean("textEditor");
+        textEditor.check();
+    }
+}
---
+++ EAD-Session7 Assignment-3 september/EAD-Session7-Spring-AutowireByName/src/textEditor/TextEditor.java
@@ -0,0 +1,32 @@
+package textEditor;
+
+public class TextEditor
+{
+    private SpellChecker spellChecker;
+    private String word;
+    
+    public String getWord()
+    {
+        return word;
+    }
+
+    public void setWord(String word)
+    {
+        this.word = word;
+    }
+
+    public void setSpellChecker(SpellChecker spellChecker)
+    {
+        this.spellChecker = spellChecker;
+    }
+
+    public SpellChecker getSpellChecker()
+    {
+        return spellChecker;
+    }
+
+    public void check()
+    {
+        System.out.println(spellChecker.checkSpelling() + " of " + getWord());
+    }
+}
